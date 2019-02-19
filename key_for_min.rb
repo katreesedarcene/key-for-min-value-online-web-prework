7f2a650f59fb76ec5b4b5ea0b9825b50abcf86e6
Kat B <katreesebarnes@gmail.com>
2019-02-19 22:11:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
     
     name_hash.each do |name, num|
       if num < 2 && :name == num 
-        binding.pry
+        
          name_hash[:name]
     end 
       end
