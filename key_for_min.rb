34e856666940057781d25218d5009a1cc3617182
Kat B <katreesebarnes@gmail.com>
2019-02-19 22:49:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,10 @@ require "pry"
 def key_for_min_value(name_hash)
        output = {}
        x = nil
-      name_hash.each do |key, value|
-        if name_hash[key] < x  && x == name_hash[x]
+      name_hash.each do |name, value|
+        if name_hash[name] < x  && x == name_hash[x]
         
-       output << key
+       output << name
        return output
      end 
         end
