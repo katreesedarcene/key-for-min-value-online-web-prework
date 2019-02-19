9dfd97d886403302242f4285daa4af3602129851
Kat B <katreesebarnes@gmail.com>
2019-02-19 22:48:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,13 @@ require "pry"
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+       output = {}
        x = nil
       name_hash.each do |key, value|
         if name_hash[key] < x  && x == name_hash[x]
         
-       return key
+       output << key
+       return output
      end 
         end
 end
