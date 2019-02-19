a3c77549bc20222e757738c9d4fdfc2cb1a38eef
Kat B <katreesebarnes@gmail.com>
2019-02-19 22:55:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,10 @@ def key_for_min_value(name_hash)
        x = nil
        name_hash.each do |key, value| 
   if x == nil 
-      min_value = key 
-    elsif value.to_i < name_hash[min_value].to_i
-      min_value = key 
+     x = key 
+    elsif value.to_i < name_hash[x].to_i
+      x = key 
     end 
   end
-  min_value
+  x
 end
